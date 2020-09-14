c9f746a74fd6c0b98f20ab8c3fc4693c51d00633
Boom3k-XPS15-9500 <boomboom3k@gmail.com>
2020-09-13 23:59:17 -0400

    Added ByteCount methods

--- Utils4Go.go
+++ Utils4Go.go
@@ -121,6 +121,32 @@ func GetAllFiles(root string) []string {
 	}
 	return files
 }
+func ByteCount(b int64) string {
+	const unit = 1000
+	if b < unit {
+		return fmt.Sprintf("%d B", b)
+	}
+	div, exp := int64(unit), 0
+	for n := b / unit; n >= unit; n /= unit {
+		div *= unit
+		exp++
+	}
+	return fmt.Sprintf("%.1f %cB",
+		float64(b)/float64(div), "kMGTPE"[exp])
+}
+func ByteCountIEC(b int64) string {
+	const unit = 1024
+	if b < unit {
+		return fmt.Sprintf("%d B", b)
+	}
+	div, exp := int64(unit), 0
+	for n := b / unit; n >= unit; n /= unit {
+		div *= unit
+		exp++
+	}
+	return fmt.Sprintf("%.1f %ciB",
+		float64(b)/float64(div), "KMGTPE"[exp])
+}
 
 //Console Stuff-----------------------------------------------------------------------
 func Readline(output string) string {
